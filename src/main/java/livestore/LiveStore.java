78660c6cf7910bb77c5f0790ae585771d03277d0
ARIT <ARIT@ARIT-5Y5ZPR1.arit.co.th>
2017-07-19 13:56:53 +0700

    Add Server service

--- src/main/java/livestore/LiveStore.java
+++ src/main/java/livestore/LiveStore.java
@@ -1,6 +1,11 @@
-
 package livestore;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 
 /**
@@ -12,17 +17,29 @@ public class LiveStore {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         StoreLoader storeLoader = new StoreLoader(
-                "mydb1","root","password");
-        List<ItemDTO> items = storeLoader.LoadItems();  
-        
-        for( ItemDTO item : items) {
-            System.out.println( item.getType() 
-                    + ", sellprice = " 
-                    + item.getCost() * item.getPrice());
+                "mydb1", "root", "password");
+        List<ItemDTO> items = storeLoader.LoadItems();
+
+        createItemService(items);
+    }
+
+    public static void createItemService(List<ItemDTO> items) throws IOException {
+        String clientSentence;
+        String capSentence;
+
+        ServerSocket welcomeSocket = new ServerSocket(8080);
+
+        while (true) {
+            Socket conSocket = welcomeSocket.accept();
+            BufferedReader fromClient = new BufferedReader (
+                new InputStreamReader(conSocket.getInputStream()));
+            DataOutputStream toClient = new DataOutputStream(conSocket.getOutputStream());
+            clientSentence = fromClient.readLine();
+            String s = "Hello,"+ clientSentence+ '\n';
+            toClient.writeBytes(s);  
         }
     }
-    
 }
